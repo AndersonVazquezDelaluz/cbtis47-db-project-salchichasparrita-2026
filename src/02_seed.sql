4fc83618ed97898bd565131434191c32865655d2
AxelGabrieldelaCruzOsorio <delacruz.osorio.axel.gabriel.m3s1@gmail.com>
2026-04-15 13:46:27 -0600

    Update 02_seed.sql

--- src/02_seed.sql
+++ src/02_seed.sql
@@ -1,1 +1,7 @@
-
+INSERT INTO `USER` (name, email, password_hash)
+VALUES
+  ('Anuar Contreras', 'sorro595uwuolo@gmail.com', '$2a$10$FlZcSBjaH7J1hdLXO38mn.R.G6SqL6w3.ifXAo8ioB9GeA6etKzLK'),
+  ('Anderson de la luz', 'vazquezdllandry@gmail.com', '$2a$10$FlZcSBjaH7J1hdLXO38mn.6vckgDEDEv2qPmu3u7o6c1mA2EgupUW'),
+  ('Jayden Sarmiento', 'ivanspidey11@gmail.com', '$2a$10$FlZcSBjaH7J1hdLXO38mn.Gj66BAkYaZNJA7OZ/thb4I6w6OV4nCC'),
+  ('Axel de la Cruz', 'axeld7973@fmail.com', '$2a$10$FlZcSBjaH7J1hdLXO38mn.B85oRuyvU3iQ4QyBCOUOmDmNyHGSTd.'),
+  ('Matthew vergasrojas', 'venegasrojasmatthewelias@gmail.com', '$2a$10$FlZcSBjaH7J1hdLXO38mn.zJcmK/9g1q0OvQMUBon3DpaxSf3XW42');
